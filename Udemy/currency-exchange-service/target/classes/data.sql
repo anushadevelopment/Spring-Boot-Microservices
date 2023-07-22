21ca16c1c284c0a10b834a66ae27a8a82320d3b7
anusha <anushadevelopement@gmail.com>
2023-07-22 12:40:27 +0800

    -

---
+++ Udemy/currency-exchange-service/target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'SGD', 65, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'EUR', 'SGD', 75, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'AUD', 'SGD', 25, '')
